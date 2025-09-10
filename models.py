380d2388f6e38ddfa3fbbebfdcfe7ebe9be270a2
scaliann <mishah238@gmail.com>
2025-09-10 10:38:42 +0300

    commit

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Task(Base):
     __tablename__ = "tasks"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String, nullable=False)
-    title_ci: Mapped[str] = mapped_column(String, nullable=False, index= True)
+    title_ci: Mapped[str] = mapped_column(String, nullable=False, index = True)
     done: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), index = True)
     updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
